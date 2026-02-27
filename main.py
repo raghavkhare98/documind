712037b64c7546402004ef4e0b6e4d1d1b414793
raghavkhare98 <khare08rma@gmail.com>
2026-02-26 19:32:07 -0500

    Update main to run Indexing Pipeline on specific doc type

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-from app.text_chunker import DocumentChunker
-from app.ingestion.pipeline import IngestionPipeline
+from app.ingestion.pipeline import IngestionPipeline, IndexingPipeline
 
 def main():
 
-    pipeline = IngestionPipeline()
-    doc = pipeline.ingest_document("/Users/raghavkhare/repos/documind/data/raw/research_papers/blended_rag.pdf")
-    
-    chunker = DocumentChunker(text=doc["text"], doc_id=doc["doc_id"], metadata=doc["metadata"])
-    chunks = chunker.chunk()
-    
+    indexing_pipeline = IndexingPipeline()
+    summary = indexing_pipeline.index_directory("./data/raw/documentations", skip_existing=True, store_in_milvus=True)
+    print(summary)
 
 if __name__ == "__main__":
     main()
+
